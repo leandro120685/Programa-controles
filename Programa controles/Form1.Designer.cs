ecdce15c0c3dfe2482be0c93dd3ad53fc883b229
leandro120685 <leandro120685@gmail.com>
2023-07-28 07:36:55 -0300

    Actualización Grafica

--- Programa controles/Form1.Designer.cs
+++ Programa controles/Form1.Designer.cs
@@ -225,7 +225,7 @@ namespace Programa_controles
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(129, 23);
             this.button5.TabIndex = 31;
-            this.button5.Text = "Guardar Qr";
+            this.button5.Text = "Guardar QR";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
